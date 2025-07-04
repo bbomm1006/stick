ca29c4e8bd6af01526ddeb3f2ecdc98056abc60d
bbomm1006 <bbomm1006@gmail.com>
2025-07-04 14:00:18 +0900

    리사이징

--- stick-clone/src/main.js
+++ stick-clone/src/main.js
@@ -2,4 +2,22 @@ import { createApp } from 'vue'
 import './styles/main.scss'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+
+// 가로 리사이징 감지 후 새로고침
+let previousWidth = window.innerWidth
+let resizeTimeout
+
+window.addEventListener('resize', () => {
+  const currentWidth = window.innerWidth
+
+  if (previousWidth !== currentWidth) {
+    clearTimeout(resizeTimeout)
+    resizeTimeout = setTimeout(() => {
+      location.reload()
+    }, 500) // 0.5초 후 새로고침
+  }
+
+  previousWidth = currentWidth
+})
